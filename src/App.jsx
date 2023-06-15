05eeeb475ed3d53f6af7eae8b6a34e34f499b30a
Henry <codigomentor@gmail.com>
2023-06-14 21:03:39 -0600

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { data } from './assets/data';
+
+export const App = () => {
+  const listRef = useRef();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+
+  useEffect(() => {
+    const listNode = listRef.current;
+    const imgNode = listNode.querySelectorAll("li > img")[currentIndex];
+
+    if (imgNode) {
+      imgNode.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+
+  }, [currentIndex]);
+
+
+  const scrollToImage = (direction) => {
+    if (direction === 'prev') {
+      setCurrentIndex(curr => {
+        const isFirstSlide = currentIndex === 0;
+        return isFirstSlide ? 0 : curr - 1;
+      })
+    } else {
+      const isLastSlide = currentIndex === data.length - 1;
+      if (!isLastSlide) {
+        setCurrentIndex(curr => curr + 1);
+      }
+    }
+  }
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  }
+
+  return (
+    <div className="main-container">
+      <div className="slider-container">
+        <div className='leftArrow' onClick={() => scrollToImage('prev')}>&#10092;</div>
+        <div className='rightArrow' onClick={() => scrollToImage('next')}>&#10093;</div>
+        <div className="container-images">
+          <ul ref={listRef}>
+            {
+              data.map((item) => {
+                return <li key={item.id}>
+                  <img src={item.imgUrl} width={500} height={280} />
+                </li>
+              })
+            }
+          </ul>
+        </div>
+        <div className="dots-container">
+          {
+            data.map((_, idx) => (
+              <div key={idx}
+                className={`dot-container-item ${idx === currentIndex ? "active" : ""}`}
+                onClick={() => goToSlide(idx)}>
+                &#9865;
+              </div>))
+          }
+        </div>
+      </div>
+    </div >
+  )
+}
---
+++ src/assets/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    id: 1, imgUrl: "src/assets/images/angular.jpeg"
+  },
+  {
+    id: 2, imgUrl: "src/assets/images/redux_angular.jpg"
+  },
+  {
+    id: 3, imgUrl: "src/assets/images/php-login.jpeg"
+  },
+  {
+    id: 4, imgUrl: "src/assets/images/jquery.jpeg"
+  },
+  {
+    id: 5, imgUrl: "src/assets/images/node-chat.jpeg"
+  }
+];
